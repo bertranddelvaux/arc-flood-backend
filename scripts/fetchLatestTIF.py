a84fa1cd5e181fceb26b47e81a74bcd51f001311
bertranddelvaux <bertrand.delvaux@gmail.com>
2022-05-16 19:10:46 +0700

    1st commit

---
+++ scripts/fetchLatestTIF.py
@@ -0,0 +1,30 @@
+#####################################################
+# Author: Bertrand Delvaux (2022)                   #
+#                                                   #
+# Script to mirror JBA's data to ARC backend server #
+#                                                   #
+#####################################################
+
+import os
+import pysftp
+
+
+HOSTNAME = 'sftp.floodforesight.com'
+USERNAME = os.environ['JBA_USERNAME']
+PASSWORD = os.environ['JBA_PASSWORD']
+
+with pysftp.Connection(host=HOSTNAME, username=USERNAME, password=PASSWORD) as sftp:
+    print("Connection successfully established ... ")
+    # Switch to a remote directory
+    sftp.cwd('moz')
+    sftp.cwd('raster')
+    sftp.cwd('2022')
+    sftp.cwd('05')
+    sftp.cwd('15')
+
+    # Obtain structure of the remote directory '/opt'
+    directory_structure = sftp.listdir_attr()
+
+    # Print data
+    for attr in directory_structure:
+        print(attr.filename, attr)
