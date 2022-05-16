ba3e02f94d1ae957416759c87008d6c317003692
bertranddelvaux <bertrand.delvaux@gmail.com>
2022-05-16 19:40:31 +0700

    1st structure of the folders

--- scripts/fetchLatestTIF.py
+++ scripts/fetchLatestTIF.py
@@ -13,9 +13,32 @@ HOSTNAME = 'sftp.floodforesight.com'
 USERNAME = os.environ['JBA_USERNAME']
 PASSWORD = os.environ['JBA_PASSWORD']
 
+def createFolderIfNotExists(folder):
+    if not(os.path.exists(folder)):
+        print(f'Creating {folder} folder')
+        os.makedirs(folder)
+    # switch to folder
+    os.chdir(folder)
+    return
+
 # opening sftp connection
 with pysftp.Connection(host=HOSTNAME, username=USERNAME, password=PASSWORD) as sftp:
     print("Connection successfully established ... ")
+
+    # check if data folder exists
+    folder = 'data'
+    createFolderIfNotExists(folder)
+    root_folder = os.path.abspath(os.getcwd())
+
+    # list countries
+    countries_list = sftp.listdir_attr()
+    for country in countries_list:
+        os.chdir(root_folder)
+        folder = country.filename
+        print(f'\tFound country {folder}')
+        createFolderIfNotExists(folder)  # check if country exists
+        # back to main folder
+
     # Switch to a remote directory
     sftp.cwd('moz')
     sftp.cwd('raster')
