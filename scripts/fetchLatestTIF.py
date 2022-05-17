634bda28c681ebb7bb578fc2dc64c08f136791bc
bertranddelvaux <bertrand.delvaux@gmail.com>
2022-05-17 19:18:42 +0700

    Mirror sftp repo

--- scripts/fetchLatestTIF.py
+++ scripts/fetchLatestTIF.py
@@ -35,37 +35,6 @@ with pysftp.Connection(host=HOSTNAME, username=USERNAME, password=PASSWORD) as s
     data = 'data'
     createFolderIfNotExists(data, n)  # check if data folder exists
     data_path = os.path.abspath(os.getcwd())  # get absolute path for future reference
-    # sftp
-    sftp.chdir('.')
-    sftp_data_path = os.path.abspath(sftp.getcwd())
 
-    # list countries
-    countries = sftp.listdir() # [c.filename for c in sftp.listdir_attr()]
-    n = 1
-    for country in countries:
-        # local
-        os.chdir(data_path)  # switch to root_folder
-        print(f'{indent(n)}Found country {country}')
-        createFolderIfNotExists(country, n)  # check if country exists
-        country_path = os.path.abspath(os.getcwd())  # get absolute path for future reference
-        # sftp
-        sftp.chdir(sftp_data_path)
-        sftp.chdir(country)  # switch sftp to current folder
-        sftp_country_path = os.path.abspath(sftp.getcwd())
-
-
-
-
-    # # Switch to a remote directory
-    # sftp.cwd('moz')
-    # sftp.cwd('raster')
-    # sftp.cwd('2022')
-    # sftp.cwd('05')
-    # sftp.cwd('15')
-    #
-    # # Obtain structure of the remote directory '/opt'
-    # directory_structure = sftp.listdir_attr()
-    #
-    # # Print data
-    # for attr in directory_structure:
-    #     print(attr.filename, attr)
+    sftp.get_r('/', data_path, preserve_mtime=False)
+    #sftp.get_r('/moz/raster/2022/04/28', data_path, preserve_mtime=False)
